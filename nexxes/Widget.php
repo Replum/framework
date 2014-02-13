d8db9d99d7f82ef3acadd3f76d27c433023a2d90
Dennis Birkholz <dennis@birkholz.biz>
2014-02-13 08:24:09 +0100

    Smarty helper function and call (un)serialization functions

--- nexxes/Widget.php
+++ nexxes/Widget.php
@@ -7,11 +7,12 @@ use \nexxes\property\Config;
 /**
  * Base class that can be used for all widgets
  */
-class Widget implements iWidget, \Serializable {
+abstract class Widget implements iWidget, \Serializable {
 	use property\ChangeMonitoringTrait;
 	use property\UpdateValuesTrait;
 	
 	public function __construct() {
+		$this->id = PageContext::$widgetRegistry->register($this);
 		$this->_initializeChangeMonitoring();
 	}
 	
@@ -19,4 +20,27 @@ class Widget implements iWidget, \Serializable {
 	 * @Config(type="string")
 	 */
 	public $id;
+	
+	
+	/**
+	 * Return a clean smarty instance
+	 * 
+	 * @return \Smarty
+	 */
+	protected function smarty() {
+		$smarty = clone PageContext::$smarty;
+		$smarty->assign('id', $this->id);
+		$smarty->assign('widget', $this);
+		
+		return $smarty;
+	}
+	
+	public function serialize() {
+		return $this->_SerializeChangeMonitor();
+	}
+	
+	public function unserialize($serialized) {
+		$this->_UnserializeChangeMonitor($serialized);
+		$this->updateValues();
+	}
 }
