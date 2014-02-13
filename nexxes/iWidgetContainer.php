c2fc66c98c3cca63d5a386060389288b9e94af44
Dennis Birkholz <dennis@birkholz.biz>
2014-02-13 08:19:01 +0100

    Container widgets should contain children

--- nexxes/iWidgetContainer.php
+++ nexxes/iWidgetContainer.php
@@ -8,4 +8,17 @@ namespace nexxes;
  * This interface contains all methods required to manage child handling.
  */
 interface iWidgetContainer extends iWidget {
+	/**
+	 * Add a child to the container
+	 * 
+	 * @param iWidget $child
+	 */
+	public function add(iWidget $child);
+	
+	/**
+	 * Get the list of current children
+	 * 
+	 * @return array<iWidget>
+	 */
+	public function children();
 }
