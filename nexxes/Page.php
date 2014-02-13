15f1fb8e64a7af927063e6a6276717abcb8a9860
Dennis Birkholz <dennis@birkholz.biz>
2014-02-13 08:21:50 +0100

    Cleanup

--- nexxes/Page.php
+++ nexxes/Page.php
@@ -4,17 +4,8 @@ namespace nexxes;
 
 use \nexxes\PageContext;
 
-class Page extends Widget implements iPage {
+abstract class Page extends WidgetContainer implements iPage {
 	public function __construct() {
-		echo "The Page constructor<br>\n";
-		
 		$this->id = PageContext::$widgetRegistry->pageID;
-	}
-	
-	public function persist() {
-		echo "Persisting data ...<br>\n";
-		flush();
-		$data = \serialize($this);
-		\apc_store(PageContext::$widgetRegistry->pageID, $data);
 	}
 }
