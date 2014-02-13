4b814416a609e1be291827f01cdeaefd6240afe8
Dennis Birkholz <dennis@birkholz.biz>
2014-02-13 03:06:43 +0100

    Plain interface, need methods

---
+++ nexxes/iPage.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace nexxes;
+
+/**
+ * A page represents the main entity to visualize
+ */
+interface iPage extends iWidgetContainer {
+}
---
+++ nexxes/iWidgetContainer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace nexxes;
+
+/**
+ * Widgets that can contain other widgets are called containers.
+ * 
+ * This interface contains all methods required to manage child handling.
+ */
+interface iWidgetContainer extends iWidget {
+}
