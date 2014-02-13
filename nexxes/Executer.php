5884e6988ba50d8f784e9fad3a8d108dcce291a8
Dennis Birkholz <dennis@birkholz.biz>
2014-02-13 05:48:14 +0100

    Page creation/restoring improved

--- nexxes/Executer.php
+++ nexxes/Executer.php
@@ -11,10 +11,8 @@ class Executer {
 	 */
 	private $pageNamespace;
 	
-	/**
-	 * @var \nexxes\iPage
-	 */
-	private $page;
+	
+	
 	
 	public function __construct($pageNamespace = '\nexxes\pages') {
 		$this->pageNamespace = $pageNamespace;
@@ -25,33 +23,46 @@ class Executer {
 		PageContext::$smarty->setCompileDir(VENDOR_DIR . '/../tmp/smarty-compile');
 		PageContext::$smarty->setCacheDir(VENDOR_DIR . '/../tmp/smarty-cache');
 		
+		// Parse request data
+		PageContext::$request = new \nexxes\helper\RequestData();
+		
 		// Property annotation reader
 		PageContext::$propertyHandler = new \nexxes\property\Handler();
 		
-		// Get the name and class of the current page
-		$page = (isset($_REQUEST['page']) ? $_REQUEST['page'] : 'Index');
-		$class = $this->pageNamespace . '\\' . $page;
-
-		if (!\class_exists($class)) {
-			throw new \InvalidArgumentException('Invalid page "' . $page . '"!');
-		}
-		
 		// Restore persisted page
-		if (isset($_REQUEST['pid']) && (false !== ($serialized = \apc_fetch($_REQUEST['pid'])))) {
-			$this->page = \unserialize($serialized);
-			if (!($this->page instanceof $class)) {
-				throw new \InvalidArgumentException('Invalid page access!');
-			}
+		if (($pid = PageContext::$request->getPageID()) && (false !== ($serialized = \apc_fetch($pid)))) {
+			echo "Restoring page<br>\n";
+			
+			PageContext::$widgetRegistry = \unserialize(\apc_fetch($pid . '-widgets'));
+			PageContext::$page = \unserialize($serialized);
+			
+			echo "Page-ID: " . PageContext::$page->id . "<br>\n";
+			echo "From Registry: " . PageContext::$widgetRegistry->pageID . "<br>\n";
 		}
 		
 		else {
-			PageContext::$widgetRegistry = new \nexxes\WidgetRegistry();
-			$this->page = new $class();
+			// Get page from request or set default
+			if (!($page = PageContext::$request->getPage())) {
+				$page = 'Index';
+			}
+			
+			// Get the name and class of the current page
+			$class = $this->pageNamespace . '\\' . $page;
+
+			if (!\class_exists($class)) {
+				throw new \InvalidArgumentException('Invalid page "' . $page . '"!');
+			}
+			
+			PageContext::$widgetRegistry = new \nexxes\helper\WidgetRegistry();
+			PageContext::$page = new $class();
 		}
 	}
 	
+	
 	public function execute() {
-		$this->page->render();
-		$this->page->persist();
+		PageContext::$page->render();
+		
+		PageContext::$page->persist();
+		\apc_store(PageContext::$widgetRegistry->pageID . '-widgets', \serialize(PageContext::$widgetRegistry));
 	}
 }
