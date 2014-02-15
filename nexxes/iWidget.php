13d0ec5b86bfe59c8171036f220e619e9a35a4d0
Dennis Birkholz <dennis@birkholz.biz>
2014-02-15 06:09:01 +0100

    Add some helper functions required later

--- nexxes/iWidget.php
+++ nexxes/iWidget.php
@@ -10,11 +10,45 @@ interface iWidget {
 	 * The widget has changed properties
 	 * @return bool
 	 */
-	public function hasChanged();
+	function hasChanged();
 	
 	/**
 	 * Returns a list of property->newvalue pairs
 	 * @return array
 	 */
-	public function getChanges();
+	function getChanges();
+	
+	/**
+	 * Set a property of the class
+	 * 
+	 * @param string $name Name of the property to change
+	 * @param mixed $value The value to set
+	 * @return \nexxes\iWidget The widget for chaining
+	 */
+	function set($property, $value);
+	
+	/**
+	 * Add the supplied value to the array property named $property
+	 * 
+	 * @param string $property Name of the property to modify
+	 * @param mixed $value The value to add to the property
+	 * @return \nexxes\iWidget The widget for chaining
+	 */
+	function add($property, $value);
+	
+	/**
+	 * Remove the supplied value from the array property named $property
+	 * 
+	 * @param string $property Name of the property to modify
+	 * @param mixed $value The value to remove from the property
+	 * @return \nexxes\iWidget The widget for chaining
+	 */
+	function del($property, $value);
+	
+	/**
+	 * Render the HTML representation of the widget and return it as a string
+	 * 
+	 * @return string The HTML code of the widget
+	 */
+	function renderHTML();
 }
