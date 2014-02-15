58375eb261ab7228c219b81bc20749d0f4800fd7
Dennis Birkholz <dennis@birkholz.biz>
2014-02-15 06:10:26 +0100

    Method to restore non-persistable state after unserialzie

--- nexxes/iPage.php
+++ nexxes/iPage.php
@@ -6,4 +6,10 @@ namespace nexxes;
  * A page represents the main entity to visualize
  */
 interface iPage extends iWidgetContainer {
+	/**
+	 * (Re)-Initialize the widget with page-specific non-persistable data like database connections
+	 * 
+	 * @param \nexxes\iWidget $widget
+	 */
+	function initWidget(\nexxes\iWidget $widget);
 }
