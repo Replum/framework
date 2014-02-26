3320f04afcaf7536614e6c78b44b1fbba6cbdb71
Dennis Birkholz <dennis@birkholz.biz>
2014-02-26 18:59:22 +0100

    Simple widget representing a link

---
+++ src/widgets/Link.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace nexxes\widgets;
+
+use \nexxes\property\Config;
+
+class Link extends \nexxes\WidgetContainer {
+	/**
+	 * @var string
+	 * @Config(type="string")
+	 */
+	public $url;
+	
+	/**
+	 * @var string
+	 * @Config(type="string")
+	 */
+	public $target;
+	
+	
+	
+	
+	public function __construct($url, $target = null) {
+		parent::__construct();
+		
+		$this->url = $url;
+		$this->target = $target;
+	}
+	
+	public function renderHTML() {
+		return '<a href="' . $this->url . '"' . ($this->target ? ' target="' . $this->target . '"' : '') . $this->renderCommonAttributes() . '>' . $this->renderChildrenHTML() . '</a>';
+	}
+}
