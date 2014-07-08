cc9e56bab95ccd72221b68760513ee2a53859d47
Dennis Birkholz <dennis@birkholz.biz>
2014-07-08 21:15:38 +0200

    Enable autoloading from within this lib, used for e.g. tests

---
+++ autoload.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Make autoloading of composer managed dependencies available
+ * Autoloader is registered as dependency '\Composer\Autoload\ClassLoader'
+ */
+
+if (!defined('VENDOR_DIR')) {
+	// Used as stand alone
+	if (\file_exists(__DIR__ . '/vendor/autoload.php')) {
+		define('VENDOR_DIR', dirname(__DIR__ . '/vendor/autoload.php'));
+	}
+	
+	// Installed as dependency
+	elseif (\file_exists(__DIR__ . '/../../autoload.php')) {
+		define('VENDOR_DIR', dirname(__DIR__ . '/../../autoload.php'));
+	}
+	
+	else {
+		throw new \InvalidArgumentException('Can not find vendor directory, update dependencies with composer first!');
+	}
+	
+	$autoloader = require_once(VENDOR_DIR .  '/autoload.php');
+	
+	// Load dependencies injected by composer
+	\nexxes\dependency\Gateway::registerObject(\Composer\Autoload\ClassLoader::class, $autoloader);
+}
