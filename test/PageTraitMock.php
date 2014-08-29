ec0fd1fbcf4c8f8d207927cc039186acb05fb5fc
Dennis Birkholz <dennis@birkholz.biz>
2014-08-29 03:28:07 +0200

    Pull methods from WidgetTrait so PageTrait works more like an actual page

--- test/PageTraitMock.php
+++ test/PageTraitMock.php
@@ -14,75 +14,8 @@ namespace nexxes\widgets;
  * @author Dennis Birkholz <dennis.birkholz@nexxes.net>
  */
 class PageTraitMock implements PageInterface {
-	use PageTrait;
+	use PageTrait, WidgetContainerTrait, WidgetTrait;
 
-	public function getPage() {
-		return $this;
-	}
-	
 	public function __toString() {
-	}
-
-	public function addClass($class) {
-	}
-
-	public function count() {
-	}
-
-	public function delClass($class, $isRegex = false) {
-	}
-
-	public function escape($unquoted) {
-	}
-
-	public function getClasses($regex = null) {
-	}
-
-	public function getID() {
-	}
-
-	public function getIterator() {
-	}
-
-	public function getParent() {
-	}
-
-	public function getTabIndex() {
-	}
-
-	public function hasChild(WidgetInterface $widget) {
-	}
-
-	public function hasClass($class, $isRegex = false) {
-	}
-
-	public function isChanged() {
-	}
-
-	public function isRoot() {
-	}
-
-	public function offsetExists($offset) {
-	}
-
-	public function offsetGet($offset) {
-	}
-
-	public function offsetSet($offset, $value) {
-	}
-
-	public function offsetUnset($offset) {
-	}
-
-	public function setChanged($changed = true) {
-	}
-
-	public function setID($newID) {
-	}
-
-	public function setParent(WidgetInterface $newParent) {
-	}
-
-	public function setTabIndex($newTabIndex) {
 	}
 }
