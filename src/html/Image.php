eed832cece3080da0c677862e3b33dfcc8504da2
Dennis Birkholz <dennis@birkholz.biz>
2014-09-04 01:38:55 +0200

    Added some base html widgets

---
+++ src/html/Image.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace nexxes\widgets\html;
+
+use \nexxes\widgets\WidgetInterface;
+use \nexxes\widgets\WidgetTrait;
+
+/**
+ * @author Dennis Birkholz <dennis.birkholz@nexxes.net>
+ */
+class Image implements WidgetInterface {
+	use WidgetTrait;
+	
+	/**
+	 * The src URL of this image
+	 * @var string
+	 */
+	private $source;
+	
+	/**
+	 * @return string
+	 */
+	public function getSource() {
+		return $this->source;
+	}
+	
+	/**
+	 * @param string $newSource
+	 * @return \nexxes\widgets\html\Image $this for chaining
+	 */
+	public function setSource($newSource) {
+		if ($this->source !== $newSource) {
+			$this->source = $newSource;
+			$this->setChanged(true);
+		}
+		
+		return $this;
+	}
+	
+	public function __toString() {
+		return '<img src="' . $this->escape($this->getSource()) . '"' . $this->getAttributesHTML() . ' />';
+	}
+}
