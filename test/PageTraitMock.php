8b76e3584aa74ab7902ad0a06bed2521c6b3ac79
Dennis Birkholz <dennis@birkholz.biz>
2014-09-04 01:33:53 +0200

    Remove WidgetTrait to avoid method duplications

--- test/PageTraitMock.php
+++ test/PageTraitMock.php
@@ -1,21 +1,14 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace nexxes\widgets;
 
 /**
- * Description of PageTraitMock
- *
  * @author Dennis Birkholz <dennis.birkholz@nexxes.net>
  */
 class PageTraitMock implements PageInterface {
-	use PageTrait, WidgetContainerTrait, WidgetTrait;
+	use PageTrait, WidgetContainerTrait;
 
 	public function __toString() {
+		return "";
 	}
 }
