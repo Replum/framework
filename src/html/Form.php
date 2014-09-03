4bc718040067696d90a697a523ed4775f58fa46b
Dennis Birkholz <dennis@birkholz.biz>
2014-09-04 01:36:11 +0200

    Add name and action attributes, improve setParent to avoid nested form elements

--- src/html/Form.php
+++ src/html/Form.php
@@ -2,10 +2,102 @@
 
 namespace nexxes\widgets\html;
 
-use \nexxes\widgets;
+use \nexxes\widgets\WidgetContainerInterface;
+use \nexxes\widgets\WidgetContainerTrait;
+use \nexxes\widgets\WidgetInterface;
 
-class Form implements widgets\WidgetContainerInterface {
-	use widgets\WidgetContainerTrait, widgets\WidgetTrait;
+/**
+ * @author Dennis Birkholz <dennis.birkholz@nexxes.net>
+ */
+class Form implements WidgetContainerInterface {
+	use WidgetContainerTrait {
+		setParent as private setParentFromTrait;
+	}
+	
+	/**
+	 * @var string
+	 * @link http://www.w3.org/TR/html5/forms.html#attr-fs-action
+	 */
+	private $action;
+	
+	/**
+	 * @return string
+	 * @link http://www.w3.org/TR/html5/forms.html#attr-fs-action
+	 */
+	public function getAction() {
+		return $this->action;
+	}
+	
+	/**
+	 * @param string $newAction
+	 * @return \nexxes\widgets\html\Form $this for chaining
+	 * @link http://www.w3.org/TR/html5/forms.html#attr-fs-action
+	 */
+	public function setAction($newAction) {
+		if ($newAction !== $this->action) {
+			$this->action = $newAction;
+			$this->setChanged(true);
+		}
+		
+		return $this;
+	}
+	
+	
+	/**
+	 * @var string
+	 * @link http://www.w3.org/TR/html5/forms.html#attr-form-name
+	 */
+	private $name;
+	
+	/**
+	 * @return string
+	 * @link http://www.w3.org/TR/html5/forms.html#attr-form-name
+	 */
+	public function getName() {
+		return $this->action;
+	}
+	
+	/**
+	 * @param string $newName
+	 * @return \nexxes\widgets\html\Form $this for chaining
+	 * @link http://www.w3.org/TR/html5/forms.html#attr-form-name
+	 */
+	public function setName($newName) {
+		if ($newName !== $this->name) {
+			$this->name = $newName;
+			$this->setChanged(true);
+		}
+		
+		return $this;
+	}
+	
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	public function setParent(WidgetInterface $newParent) {
+		$this->setParentFromTrait($newParent);
+		
+		if ($this->isChanged()) {
+			$ancestors = \array_reverse($this->getAncestors());
+			
+			foreach ($ancestors AS $ancestor) {
+				if ($ancestor instanceof Form) {
+					throw new \InvalidArgumentException('A form element can not contain another form element, see: http://www.w3.org/TR/html5/forms.html#the-form-element');
+				}
+			}
+		}
+		
+		return $this;
+	}
+	
+	
+	/**
+	 *
+	 * @var type 
+	 */
+	private $elements;
+	
 	
 	public function __toString() {
 		$r = '<form role="form"'
