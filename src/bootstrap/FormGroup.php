445049f85ef2c390a60b8de0577ec3e6562d5945
Dennis Birkholz <dennis@birkholz.biz>
2014-09-30 19:31:19 +0200

    Use more widgets, less static html code

--- src/bootstrap/FormGroup.php
+++ src/bootstrap/FormGroup.php
@@ -6,6 +6,7 @@ use \nexxes\widgets\html\FormElement;
 use \nexxes\widgets\html\FormElementInterface;
 use \nexxes\widgets\WidgetCompositeInterface;
 use \nexxes\widgets\WidgetCompositeTrait;
+use \nexxes\widgets\WidgetContainer;
 
 /**
  * @author Dennis Birkholz <dennis.birkholz@nexxes.net>
@@ -123,40 +124,58 @@ class FormGroup extends FormElement implements FormElementInterface, WidgetCompo
 	
 	
 	
-	public function __construct(\nexxes\widgets\WidgetInterface $parent) {
-		$this->setParent($parent);
-		$this->getPage()->getWidgetRegistry()->register($this);
+	public function __construct(\nexxes\widgets\WidgetInterface $parent = null) {
+		if ($parent !== null) { $this->setParent($parent); }
 		
 		$this->childSlot('label');
 		$this->childSlot('element');
+		$this->childSlot('help');
+		
 	}
 	
 	public function __toString() {
-		$this->addClass('form-group');
+		// Formgroup should have an ID, so enforce it (late)
+		$this->setID();
 		
-		$r = '<div'
-			. $this->getAttributesHTML()
-			. '>' . PHP_EOL
-			. '<label class="control-label col-lg-2" for="' . $this['element']->getID() . '">' . $this['element']->getName() . '</label>' . PHP_EOL
-			. ($this['label'] ?: "")
-			. '<div class="col-lg-10">' . ($this['element'] ?: "")
-		;
+		$this->addClass('form-group');
+		$this->addClass('form-group-sm');
+		
+		if (isset($this['label'])) {
+			// Assign form element to label
+			if (($this['label']->getFor() === null) && isset($this['element'])) {
+				$this['label']->setFor($this['element']);
+			}
+			
+			// Set default label classes
+			$this['label']->addClass('control-label');
+			$this['label']->addClass('col-lg-3');
+		}
 		
 		if ($this->hasFeedback()) {
 			if ($this->hasSuccess()) {
-				$r .= '<span class="glyphicon glyphicon-ok form-control-feedback"></span>' . PHP_EOL;
+				$icon = (new Glyphicon('ok'))->addClass('form-control-feedback');
 			} elseif ($this->hasWarning()) {
-				$r .= '<span class="glyphicon glyphicon-warning-sign form-control-feedback"></span>' . PHP_EOL;
+				$icon = (new Glyphicon('warning-sign'))->addClass('form-control-feedback');
 			} elseif ($this->hasError()) {
-				$r .= '<span class="glyphicon glyphicon-remove sign form-control-feedback"></span>' . PHP_EOL;
+				$icon = (new Glyphicon('remove'))->addClass('form-control-feedback');
 			}
 		}
 		
-		$r .= '<span class="help-block">A block of help text that breaks onto a new line and may extend beyond one line.A block of help text that breaks onto a new line and may extend beyond one line.</span>'  . PHP_EOL
-			. '</div>'  . PHP_EOL
-			. '<script>window.setTimeout(function(){ console.log("Remove element"); $("#' . $this->getID() . '").removeClass("has-success"); $("#' . $this->getID() . ' .form-control-feedback").addClass("hidden"); }, 5000);</script>'  . PHP_EOL
-			. '</div>'  . PHP_EOL
-		;
+		if (isset($this['help'])) {
+			$this['help']->addClass('help-block');
+		}
+		
+		$inline = false;
+		
+		$r = '<div' . $this->getAttributesHTML() . '>' . PHP_EOL
+			. ($this['label'] ?: "")
+			. (!$inline ? '<div class="col-lg-9">' : '')
+			. ($this['element'] ?: "")
+			. (isset($icon) ? $icon : '')
+			. ($this['help'] ?: "")
+			. (!$inline ? '</div>' : '')
+			//. '<script>window.setTimeout(function(){ console.log("Remove element"); $("#' . $this->getID() . '").removeClass("has-"); $("#' . $this->getID() . ' .form-control-feedback").addClass("hidden"); }, 5000);</script>'  . PHP_EOL
+			. '</div>'  . PHP_EOL;
 		
 		return $r;
 	}
