7c787f728c628b15259fe5ef178df332cfc7e097
Dennis Birkholz <dennis@birkholz.biz>
2014-09-30 19:37:17 +0200

    Date input with date picker

--- src/bootstrap/Page.php
+++ src/bootstrap/Page.php
@@ -12,8 +12,11 @@ abstract class Page extends \nexxes\widgets\html\Page {
 	public function __construct() {
 		$this->addStyleSheet((new StyleSheetLink())->setUrl('/components/bootstrap/css/bootstrap.css'));
 		$this->addStyleSheet((new StyleSheetLink())->setUrl('/components/bootstrap/css/bootstrap-theme.css'));
+		$this->addStyleSheet((new StyleSheetLink())->setUrl('/components/bootstrap-datepicker/css/datepicker3.css'));
 		
 		$this->addScript((new ScriptLink())->setUrl('/components/jquery/jquery.js'));
 		$this->addScript((new ScriptLink())->setUrl('/components/bootstrap/js/bootstrap.js'));
+		$this->addScript((new ScriptLink())->setUrl('/components/bootstrap-datepicker/js/bootstrap-datepicker.js'));
+		$this->addScript((new ScriptLink())->setUrl('/components/bootstrap-datepicker/js/locales/bootstrap-datepicker.de.js'));
 	}
 }
