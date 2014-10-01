454f122bd44d5ba712dca57699aa2e2c358973b0
Dennis Birkholz <dennis@birkholz.biz>
2014-10-01 20:06:40 +0200

    Test xml property definition has a property name

--- test/pagedef/XMLImporterTest.php
+++ test/pagedef/XMLImporterTest.php
@@ -2,6 +2,8 @@
 
 namespace nexxes\widgets\pagedef;
 
+use \nexxes\widgets\PageTraitMock;
+
 /**
  * @author Dennis Birkholz <dennis.birkholz@nexxes.net>
  */
@@ -9,5 +11,17 @@ class XMLImporterTest extends StructImporterTest {
 	protected function loadInizializer($name, $root) {
 		$importer = new XMLImporter();
 		return $importer->importFile($root, __DIR__ . '/defs/' . \substr($name, 4) . '.xml');
+	}
+	
+	/**
+	 * Verify a property always has a name
+	 * @test
+	 * @expectedException \UnexpectedValueException
+	 * @expectedExceptionMessage "name" attribute required
+	 */
+	public function testPropertyWithoutName() {
+		$page = new PageTraitMock();
+		$func = $this->loadInizializer(__FUNCTION__, $page);
+		$func($page);
 	}
 }
