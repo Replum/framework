a1e049fc751f0302eca0be743ddd259aec68aa0f
Dennis Birkholz <dennis.birkholz@nexxes.net>
2014-10-12 21:07:07 +0200

    HTML button

---
+++ src/Button.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the nexxes/widgets-html package.
+ * 
+ * Copyright (c) Dennis Birkholz, nexxes Informationstechnik GmbH <dennis.birkholz@nexxes.net>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace nexxes\widgets\html;
+
+use \nexxes\widgets\WidgetContainerInterface;
+use \nexxes\widgets\WidgetContainerTrait;
+use \nexxes\widgets\WidgetHasClickEventInterface;
+use \nexxes\widgets\WidgetHasClickEventTrait;
+
+/**
+ * @author Dennis Birkholz <dennis.birkholz@nexxes.net>
+ */
+class Button implements WidgetContainerInterface, FormElementInterface, WidgetHasClickEventInterface {
+	use FormInputTrait, WidgetContainerTrait, WidgetHasClickEventTrait;
+	
+	public function __toString() {
+		return '<' . $this->getTag() . $this->renderAttributes() . '>' . $this->renderChildren() . '</button>' . PHP_EOL;
+	}
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function validTags() {
+		return [ 'button' ];
+	}
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function renderAttributes() {
+		return $this->renderWidgetAttributes()
+			. $this->renderFormInputAttributes()
+			. $this->renderClickHandler();
+	}
+}
