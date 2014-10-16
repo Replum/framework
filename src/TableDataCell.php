6394b75b076f36a1d8a02d9a56e033d77266fd58
Dennis Birkholz <dennis.birkholz@nexxes.net>
2014-10-16 18:28:47 +0200

    Enable doubleClick event for table cells

--- src/TableDataCell.php
+++ src/TableDataCell.php
@@ -13,15 +13,17 @@ namespace nexxes\widgets\html;
 
 use \nexxes\widgets\WidgetContainerInterface;
 use \nexxes\widgets\WidgetContainerTrait;
+use \nexxes\widgets\WidgetHasDoubleClickEventInterface;
+use \nexxes\widgets\WidgetHasDoubleClickEventTrait;
 
 /**
  * @author Dennis Birkholz <dennis.birkholz@nexxes.net>
  * @link http://www.w3.org/TR/html5/tabular-data.html#the-td-element
  */
-class TableDataCell implements WidgetContainerInterface, TableCellInterface {
-	use WidgetContainerTrait, TableCellTrait;
+class TableDataCell implements WidgetContainerInterface, TableCellInterface, WidgetHasDoubleClickEventInterface {
+	use WidgetContainerTrait, TableCellTrait, WidgetHasDoubleClickEventTrait;
 	
 	public function __toString() {
-		return '<td ' . $this->renderAttributes() . $this->renderTableCell() . '>' . $this->renderChildren() . '</td>' . PHP_EOL;
+		return '<td ' . $this->renderAttributes() . $this->renderTableCell() . $this->renderDoubleClickHandler() . '>' . $this->renderChildren() . '</td>' . PHP_EOL;
 	}
 }
