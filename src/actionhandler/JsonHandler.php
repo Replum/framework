9e0b73af89f09ad78c14b719de62b479bc114e14
Dennis Birkholz <dennis.birkholz@nexxes.net>
2014-12-30 01:59:02 +0100

    Catch exceptions in json handling

--- src/actionhandler/JsonHandler.php
+++ src/actionhandler/JsonHandler.php
@@ -23,45 +23,52 @@ class JsonHandler {
 	}
 	
 	public function execute() {
-		/* $var $request \Symfony\Component\HttpFoundation\Request */
-		$request = $this->executer->getRequest();
-		
-		$event = $request->request->get('nexxes_event');
-		
-		if (!\in_array($event, ['click', 'change', 'dblclick', 'submit', ])) {
-			throw new \InvalidArgumentException('Invalid event with name "' . $event . '"');
-		}
-		
-		$page_id = $request->request->get('nexxes_pid');
-		
-		/* @var $page \nexxes\widgets\PageInterface */
-		$page = \unserialize(\gzinflate(\apc_fetch($this->executer->getCacheNamespace() . '.' . $page_id)));
+		try {
+			/* $var $request \Symfony\Component\HttpFoundation\Request */
+			$request = $this->executer->getRequest();
 
-		if (!($page instanceof \nexxes\widgets\PageInterface)) {
-			throw new \RuntimeException('Can not restore page!');
-		}
+			$event = $request->request->get('nexxes_event');
 
-		$widget = $page->getWidgetRegistry()->getWidget($request->request->get('nexxes_source'));
-		if ($request->request->get('nexxes_value') !== null) {
-			$widget->setValue($request->request->get('nexxes_value'));
-		}
-		elseif ($request->request->get('nexxes_checked') !== null) {
-			$widget->setChecked($request->request->get('nexxes_checked'));
-		}
-		
-		
-		if ($event == 'click') {
-			$widget->dispatch(new WidgetOnClickEvent($widget));
-		} elseif ($event == 'change') {
-			$widget->dispatch(new WidgetOnChangeEvent($widget));
-		} elseif ($event == 'dblclick') {
-			$widget->dispatch(new WidgetOnDoubleClickEvent($widget));
-		} elseif ($event == 'submit') {
-			$widget->dispatch(new WidgetOnSubmitEvent($widget));
-		}
-		
-		$data = $this->handleChangedWidgets($page);
+			if (!\in_array($event, ['click', 'change', 'dblclick', 'submit', ])) {
+				throw new \InvalidArgumentException('Invalid event with name "' . $event . '"');
+			}
 
+			$page_id = $request->request->get('nexxes_pid');
+
+			/* @var $page \nexxes\widgets\PageInterface */
+			$page = \unserialize(\gzinflate(\apc_fetch($this->executer->getCacheNamespace() . '.' . $page_id)));
+
+			if (!($page instanceof \nexxes\widgets\PageInterface)) {
+				throw new \RuntimeException('Can not restore page!');
+			}
+
+			$widget = $page->getWidgetRegistry()->getWidget($request->request->get('nexxes_source'));
+			if ($request->request->get('nexxes_value') !== null) {
+				$widget->setValue($request->request->get('nexxes_value'));
+			}
+			elseif ($request->request->get('nexxes_checked') !== null) {
+				$widget->setChecked($request->request->get('nexxes_checked'));
+			}
+
+
+			if ($event == 'click') {
+				$widget->dispatch(new WidgetOnClickEvent($widget));
+			} elseif ($event == 'change') {
+				$widget->dispatch(new WidgetOnChangeEvent($widget));
+			} elseif ($event == 'dblclick') {
+				$widget->dispatch(new WidgetOnDoubleClickEvent($widget));
+			} elseif ($event == 'submit') {
+				$widget->dispatch(new WidgetOnSubmitEvent($widget));
+			}
+
+			$data = $this->handleChangedWidgets($page);
+		} catch (\Exception $e) {
+			$data = [[
+				'nexxes_action' => 'log',
+				'nexxes_params' => [(string)$e],
+			]];
+		}
+		
 		header('Content-Type: text/json');
 		echo json_encode($data);
 		
