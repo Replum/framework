4f87c420ebb23ffb54289ba0216e4e6563576fa2
Dennis Birkholz <dennis.birkholz@nexxes.net>
2015-01-29 08:35:57 +0100

    Apply PSR format.

--- src/FormElementInterface.php
+++ src/FormElementInterface.php
@@ -2,9 +2,9 @@
 
 /*
  * This file is part of the nexxes/widgets-html package.
- * 
+ *
  * Copyright (c) Dennis Birkholz, nexxes Informationstechnik GmbH <dennis.birkholz@nexxes.net>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -17,20 +17,22 @@ use \nexxes\widgets\WidgetInterface;
  *
  * @author Dennis Birkholz <dennis.birkholz@nexxes.net>
  */
-interface FormElementInterface extends WidgetInterface {
-	/**
-	 * Get the form this FormElement is associated with
-	 * 
-	 * @return Form
-	 */
-	function getForm();
-	
-	/**
-	 * Set the form for the FormElement
-	 * Should only be used by handlers that manage form association,
-	 * 
-	 * @param Form $form
-	 * @return FormElementInterface $this for chaining
-	 */
-	function setForm(Form $form);
+interface FormElementInterface extends WidgetInterface
+{
+
+    /**
+     * Get the form this FormElement is associated with
+     *
+     * @return Form
+     */
+    function getForm();
+
+    /**
+     * Set the form for the FormElement
+     * Should only be used by handlers that manage form association,
+     *
+     * @param Form $form
+     * @return FormElementInterface $this for chaining
+     */
+    function setForm(Form $form);
 }
